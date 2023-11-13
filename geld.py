3486cea1bd23f94f66b578d81145466c8b59db93
1exes <95047283+1exes@users.noreply.github.com>
2023-11-13 12:03:31 +0100

    Create geld.py

---
+++ geld.py
@@ -0,0 +1,11 @@
+geld=float(input("Geben sie ihren Geldbetrag ein: "))
+bezahl_geld=float(input("Bitte geben sie ihren Betag ein denn sie bezahlen müssen: "))
+
+
+rückgeld=geld-bezahl_geld
+
+
+
+
+
+print(rückgeld)
