99bf70a21de45a85b135506153d9b26631fede1f
1exes <95047283+1exes@users.noreply.github.com>
2024-03-07 17:04:07 +0100

    Update Webautomation.py

--- Web-automation/Webautomation.py
+++ Web-automation/Webautomation.py
@@ -1,23 +1,22 @@
-from playwright.sync_api import Playwright, sync_playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://dashboard.honeygain.com/")
-    page.goto("https://dashboard.honeygain.com/login")
+    page.goto("https://www.google.com/search?q=Honeygain&oq=Honeygain&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDYxMDNqMGoyqAIAsAIA&sourceid=chrome&ie=UTF-8")
+    page.get_by_role("button", name="Alle akzeptieren").click()
+    page.get_by_role("link", name="Home | Honeygain Honeygain").click()
+    page.get_by_role("button", name="Accept selected").click()
+    page.get_by_role("link", name="Login").click()
     page.get_by_placeholder("honeygain@example.com").click()
     page.get_by_placeholder("honeygain@example.com").fill("edgar.richter05@gmail.com")
     page.get_by_label("Password").click()
-    page.get_by_label("Password").fill("testhoney")
-    page.get_by_label("Password").press("Enter")
-    page.get_by_role("main").get_by_role("button").first.click()
-    page.get_by_label("Password").click()
     page.get_by_label("Password").fill("honeygaintest")
     page.get_by_role("button", name="Continue with email").click()
-    page.get_by_role("button", name="Accept all").click()
-    page.get_by_role("main").locator("img").first.click()
+    page.get_by_role("banner").get_by_role("button").nth(2).click()
+    page.locator(".sc-bdnyFh > svg").first.click()
 
     # ---------------------
     context.close()
