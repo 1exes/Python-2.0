810d8dc9229db18fd0625494343e446484ae9fc5
1exes <95047283+1exes@users.noreply.github.com>
2024-06-26 21:27:13 +0200

    geht

--- Dateinen_sortiren/wie_das_geht.py
+++ Dateinen_sortiren/wie_das_geht.py
@@ -4,6 +4,7 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from datetime import datetime, timedelta
+import threading
 
 # Quellordner und Zielordner
 source_dirs = [
@@ -38,13 +39,23 @@ protected_dirs = [
     backup_dir
 ]
 
+# Logging-Konfiguration
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+# Funktion zum Erstellen der Zielordner, falls sie nicht existieren
 def create_directories():
     directories = [dest_dir_sfx, dest_dir_music, dest_dir_video, dest_dir_image, dest_dir_documents, dest_dir_misc, archive_dir, backup_dir]
     for directory in directories:
         if not os.path.exists(directory):
-            os.makedirs(directory)
-            logging.info(f"Created directory: {directory}")
+            try:
+                os.makedirs(directory)
+                logging.info(f"Created directory: {directory}")
+            except OSError as e:
+                logging.error(f"Failed to create directory: {directory}. Error: {str(e)}")
 
+# Funktion zur Erstellung eines eindeutigen Dateinamens im Zielordner, um Duplikate zu vermeiden
 def make_unique(dest, name):
     filename, extension = os.path.splitext(name)
     counter = 1
@@ -53,118 +64,151 @@ def make_unique(dest, name):
         counter += 1
     return name
 
+# Funktion zum Verschieben einer Datei in einen Zielordner und zur Vermeidung von Duplikaten
 def move_file(dest, entry, name):
     dest_path = os.path.join(dest, name)
-    if os.path.exists(dest_path):
-        unique_name = make_unique(dest, name)
-        old_name = os.path.join(dest, name)
-        new_name = os.path.join(dest, unique_name)
-        os.rename(old_name, new_name)
-    shutil.move(entry, os.path.join(dest, name))
-    logging.info(f"Moved file: {name} to {dest}")
+    try:
+        if os.path.exists(dest_path):
+            unique_name = make_unique(dest, name)
+            old_name = os.path.join(dest, name)
+            new_name = os.path.join(dest, unique_name)
+            os.rename(old_name, new_name)
+        shutil.move(entry, os.path.join(dest, name))
+        logging.info(f"Moved file: {name} to {dest}")
+    except (shutil.Error, OSError) as e:
+        logging.error(f"Failed to move file {name} to {dest}. Error: {str(e)}")
 
-def backup_file(src):
-    backup_name = os.path.join(backup_dir, os.path.basename(src))
-    shutil.copy(src, backup_name)
-    logging.info(f"Backed up file: {os.path.basename(src)} to {backup_name}")
-
+# Funktion zum Archivieren alter Dateien in einem ZIP-Archiv
 def archive_old_files():
     now = datetime.now()
     cutoff_date = now - timedelta(days=archive_threshold_days)
     for dest_dir in [dest_dir_sfx, dest_dir_music, dest_dir_video, dest_dir_image, dest_dir_documents, dest_dir_misc]:
-        for entry in os.scandir(dest_dir):
-            if entry.is_file() and datetime.fromtimestamp(entry.stat().st_mtime) < cutoff_date:
-                archive_name = os.path.join(archive_dir, entry.name)
-                shutil.make_archive(archive_name, 'zip', dest_dir, entry.name)
-                os.remove(entry.path)
-                logging.info(f"Archived file: {entry.name}")
+        try:
+            for entry in os.scandir(dest_dir):
+                if entry.is_file() and datetime.fromtimestamp(entry.stat().st_mtime) < cutoff_date:
+                    archive_name = os.path.join(archive_dir, entry.name)
+                    shutil.make_archive(archive_name, 'zip', dest_dir, entry.name)
+                    os.remove(entry.path)
+                    logging.info(f"Archived file: {entry.name}")
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Failed to archive files in {dest_dir}. Error: {str(e)}")
 
+# Funktion zum Bereinigen leerer Ordner in den Quellordnern
 def cleanup_empty_folders():
     for source_dir in source_dirs:
-        for dirpath, dirnames, filenames in os.walk(source_dir, topdown=False):
-            if not dirnames and not filenames:
-                # Überprüfen, ob der Ordner älter als 1 Minute ist, um versehentlich erstellte Ordner zu vermeiden
-                if not is_recently_created(dirpath) and dirpath not in protected_dirs:
-                    os.rmdir(dirpath)
-                    logging.info(f"Deleted empty folder: {dirpath}")
+        try:
+            for dirpath, dirnames, filenames in os.walk(source_dir, topdown=False):
+                if not dirnames and not filenames:
+                    if not is_recently_created(dirpath) and dirpath not in protected_dirs:
+                        os.rmdir(dirpath)
+                        logging.info(f"Deleted empty folder: {dirpath}")
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Failed to clean up empty folders in {source_dir}. Error: {str(e)}")
 
+# Funktion zur Überprüfung, ob ein Ordner kürzlich erstellt wurde
 def is_recently_created(folder):
-    one_minute_ago = datetime.now() - timedelta(minutes=1)
-    folder_created_time = datetime.fromtimestamp(os.path.getctime(folder))
-    return folder_created_time > one_minute_ago
+    try:
+        one_minute_ago = datetime.now() - timedelta(minutes=1)
+        folder_created_time = datetime.fromtimestamp(os.path.getctime(folder))
+        return folder_created_time > one_minute_ago
+    except OSError as e:
+        logging.error(f"Error checking folder creation time for {folder}. Error: {str(e)}")
+        return False
 
+
+# Klasse für den FileSystemEventHandler zum Überwachen und Verschieben von Dateien
 class MoverHandler(FileSystemEventHandler):
     def on_modified(self, event):
         for source_dir in source_dirs:
-            for entry in os.scandir(source_dir):
-                name = entry.name
-                if entry.is_file():
-                    if not self.check_audio_files(entry, name) and \
-                       not self.check_video_files(entry, name) and \
-                       not self.check_image_files(entry, name) and \
-                       not self.check_document_files(entry, name):
-                        self.move_misc_files(entry, name)
+            try:
+                for entry in os.scandir(source_dir):
+                    name = entry.name
+                    if entry.is_file():
+                        if not self.check_audio_files(entry, name) and \
+                           not self.check_video_files(entry, name) and \
+                           not self.check_image_files(entry, name) and \
+                           not self.check_document_files(entry, name):
+                            self.move_misc_files(entry, name)
+            except (shutil.Error, OSError) as e:
+                logging.error(f"Error scanning directory {source_dir}. Error: {str(e)}")
 
     def check_audio_files(self, entry, name):
-        for audio_extension in audio_extensions:
-            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
-                if entry.stat().st_size < 10_000_000 or "SFX" in name:
-                    dest = dest_dir_sfx
-                else:
-                    dest = dest_dir_music
-                backup_file(entry.path)  # Sicherheitskopie erstellen
-                move_file(dest, entry.path, name)
-                logging.info(f"Moved audio file: {name} to {dest}")
-                return True
-        return False
+        try:
+            for audio_extension in audio_extensions:
+                if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                    if entry.stat().st_size < 10_000_000 or "SFX" in name:
+                        dest = dest_dir_sfx
+                    else:
+                        dest = dest_dir_music
+                    move_file(dest, entry.path, name)
+                    logging.info(f"Moved audio file: {name} to {dest}")
+                    return True
+            return False
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Error moving audio file {name}. Error: {str(e)}")
+            return False
 
     def check_video_files(self, entry, name):
-        for video_extension in video_extensions:
-            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
-                backup_file(entry.path)  # Sicherheitskopie erstellen
-                move_file(dest_dir_video, entry.path, name)
-                logging.info(f"Moved video file: {name} to {dest_dir_video}")
-                return True
-        return False
+        try:
+            for video_extension in video_extensions:
+                if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                    move_file(dest_dir_video, entry.path, name)
+                    logging.info(f"Moved video file: {name} to {dest_dir_video}")
+                    return True
+            return False
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Error moving video file {name}. Error: {str(e)}")
+            return False
 
     def check_image_files(self, entry, name):
-        for image_extension in image_extensions:
-            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
-                backup_file(entry.path)  # Sicherheitskopie erstellen
-                move_file(dest_dir_image, entry.path, name)
-                logging.info(f"Moved image file: {name} to {dest_dir_image}")
-                return True
-        return False
+        try:
+            for image_extension in image_extensions:
+                if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                    move_file(dest_dir_image, entry.path, name)
+                    logging.info(f"Moved image file: {name} to {dest_dir_image}")
+                    return True
+            return False
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Error moving image file {name}. Error: {str(e)}")
+            return False
 
     def check_document_files(self, entry, name):
-        for document_extension in document_extensions:
-            if name.endswith(document_extension) or name.endswith(document_extension.upper()):
-                backup_file(entry.path)  # Sicherheitskopie erstellen
-                move_file(dest_dir_documents, entry.path, name)
-                logging.info(f"Moved document file: {name} to {dest_dir_documents}")
-                return True
-        return False
+        try:
+            for document_extension in document_extensions:
+                if name.endswith(document_extension) or name.endswith(document_extension.upper()):
+                    move_file(dest_dir_documents, entry.path, name)
+                    logging.info(f"Moved document file: {name} to {dest_dir_documents}")
+                    return True
+            return False
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Error moving document file {name}. Error: {str(e)}")
+            return False
 
     def move_misc_files(self, entry, name):
-        dest = dest_dir_misc
-        move_file(dest, entry.path, name)
-        logging.info(f"Moved miscellaneous file: {name} to {dest}")
+        try:
+            move_file(dest_dir_misc, entry.path, name)
+            logging.info(f"Moved miscellaneous file: {name} to {dest_dir_misc}")
+        except (shutil.Error, OSError) as e:
+            logging.error(f"Error moving miscellaneous file {name}. Error: {str(e)}")
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    create_directories()
+# Funktion zur Initialisierung und Überwachung der Verzeichnisänderungen
+def start_monitoring():
     event_handler = MoverHandler()
     observer = Observer()
     for source_dir in source_dirs:
         observer.schedule(event_handler, source_dir, recursive=True)
     observer.start()
+    logging.info(f"Monitoring started for directories: {', '.join(source_dirs)}")
     try:
         while True:
-            cleanup_empty_folders()
             archive_old_files()
-            observer.join(10)  # Observer alle 10 Sekunden überprüfen
+            cleanup_empty_folders()
+            threading.Event().wait(60)
     except KeyboardInterrupt:
         observer.stop()
+        logging.info("Monitoring stopped by user")
     observer.join()
+
+if __name__ == "__main__":
+    create_directories()
+    start_monitoring()
