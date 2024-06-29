9f125918e7968481453ee91691fe52c9a7b5b191
1exes <95047283+1exes@users.noreply.github.com>
2024-06-29 12:44:58 +0200

    Neu instergram bot

--- automationen/Instergram/insta.py
+++ automationen/Instergram/insta.py
@@ -1,20 +1,59 @@
-from instabot import Bot
+import glob
 import os
+import sys
+import time
+from io import open
 
-# Instagram Anmeldedaten
-username = 'dein_username'
-password = 'dein_passwort'
+sys.path.append(os.path.join(sys.path[0], "../../"))
+from instabot import Bot  # noqa: E402
 
-# Pfad zum Video und Beschreibung
-video_path = 'pfad/zum/video.mp4'
-caption = 'Deine Videobeschreibung'
+posted_pic_list = []
+try:
+    with open("pics.txt", "r", encoding="utf8") as f:
+        posted_pic_list = f.read().splitlines()
+except Exception:
+    posted_pic_list = []
 
-# Instagram Bot initialisieren und einloggen
+timeout = 24 * 60 * 60  # pics will be posted every 24 hours
+
 bot = Bot()
-bot.login(username=username, password=password)
+bot.login()
 
-# Video hochladen
-bot.upload_video(video_path, caption=caption)
+while True:
+    folder_path = "./pics"
+    pics = glob.glob(folder_path + "/*.jpg")
+    pics = sorted(pics)
+    try:
+        for pic in pics:
+            if pic in posted_pic_list:
+                continue
 
-# Logout nach dem Hochladen
-bot.logout()
+            pic_name = pic[:-4].split("-")
+            pic_name = "-".join(pic_name[1:])
+
+            print("upload: " + pic_name)
+
+            description_file = folder_path + "/" + pic_name + ".txt"
+
+            if os.path.isfile(description_file):
+                with open(description_file, "r") as file:
+                    caption = file.read()
+            else:
+                caption = pic_name.replace("-", " ")
+
+            bot.upload_photo(pic, caption=caption)
+            if bot.api.last_response.status_code != 200:
+                print(bot.api.last_response)
+                # snd msg
+                break
+
+            if pic not in posted_pic_list:
+                posted_pic_list.append(pic)
+                with open("pics.txt", "a", encoding="utf8") as f:
+                    f.write(pic + "\n")
+
+            time.sleep(timeout)
+
+    except Exception as e:
+        print(str(e))
+    time.sleep(60)
